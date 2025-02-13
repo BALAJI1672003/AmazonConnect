ad63441cc40ce7db4a4a991a2ad5ce061ab984c6
Balaji <abalaji1672003@gmail.com>
2025-02-13 12:24:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const users=require('./user')
+const app=express();
+app.use(express.json());
+app.get('/getUser',(req,res)=>{
+    const phoneNumber=req.query.phoneNumber;
+    if(!phoneNumber){
+        res.status(400).send("Phone number is required");
+    }
+    const userName=users[phoneNumber];
+    if(userName){
+        res.status(200).json({name: userName});
+    }
+    else{
+        res.status(404).send("User not found");
+    }
+})
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
---
+++ user.js
@@ -0,0 +1,5 @@
+const users={
+    "+18006183088":"Balaji",
+    "7457487689":"sanjay"
+}
+module.exports=users;
